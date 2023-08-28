deb16600abf171ff2ee394386734f69496ecaea1
mioamiao12121 <139939501+mioamiao12121@users.noreply.github.com>
2023-08-28 11:02:49 +0800

    Create 掩膜.py

---
+++ 基础读取/掩膜.py
@@ -0,0 +1,33 @@
+"""
+掩膜是用选定的图像、图形或物体，对要处理的图像进行遮挡，来控制图像 处理的区域。
+掩膜是由0和1组成一个二进制图像，利用该掩膜图像要处理的图像进行掩膜，
+其中1值的区域被处理，0 值区域被屏蔽，不会处理
+"""
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+# 1. 直接以灰度图的方式读入
+img = cv.imread('./image/cat.jpeg',0)
+# 2. 创建蒙版
+mask = np.zeros(img.shape[:2], np.uint8)
+mask[400:650, 200:500] = 255
+# 3.掩模
+masked_img = cv.bitwise_and(img,img,mask = mask)
+# 4. 统计掩膜后图像的灰度图
+mask_histr = cv.calcHist([img],[0],mask,[256],[1,256])
+# 5. 图像展示
+fig,axes=plt.subplots(nrows=2,ncols=2,figsize=(10,8))
+axes[0,0].imshow(img,cmap=plt.cm.gray)
+axes[0,0].set_title("原图")
+axes[0,1].imshow(mask,cmap=plt.cm.gray)
+axes[0,1].set_title("蒙版数据")
+axes[1,0].imshow(masked_img,cmap=plt.cm.gray)
+axes[1,0].set_title("掩膜后数据")
+axes[1,1].plot(mask_histr)
+axes[1,1].grid()
+axes[1,1].set_title("灰度直方图")
+plt.show()
